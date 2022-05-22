cc5381db2d56399b786a68b8fc066d933be5c81d
Dayton Nolan <me@daytonnolan.com>
2022-05-22 00:36:17 -0700

    Initial commit

---
+++ lib/phoenix_scss.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixScss do
+  @moduledoc """
+  Documentation for `PhoenixScss`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixScss.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_scss_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixScssTest do
+  use ExUnit.Case
+  doctest PhoenixScss
+
+  test "greets the world" do
+    assert PhoenixScss.hello() == :world
+  end
+end
